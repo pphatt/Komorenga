b18e3a1f6b24ee8dc17f934802b6e23f80ea50b7
pphatt <vutienphat0809@gmail.com>
2023-08-02 21:39:29 +0700

    feat: update layout

--- Shell.xaml.cs
+++ Shell.xaml.cs
@@ -5,6 +5,8 @@ using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
+using Windows.UI;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -15,41 +17,6 @@ namespace Komorenga;
 /// </summary>
 public sealed partial class Shell : Window
 {
-    //public Shell()
-    //{
-    //    this.InitializeComponent();
-    //}
-
-    //private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
-    //{
-    //    SetContentFrame(args.SelectedItemContainer as NavigationViewItem);
-    //}
-
-    //private void SetContentFrame(NavigationViewItem item)
-    //{
-    //    if (item == null)
-    //    {
-    //        return;
-    //    }
-
-    //    if (item.Tag == null)
-    //    {
-    //        return;
-    //    }
-
-    //    if (item.Tag.ToString() == "Settings")
-    //    {
-    //        //contentFrame.Navigate(typeof(Settings));
-    //    }
-    //    else
-    //    {
-    //        contentFrame.Navigate(Type.GetType(item.Tag.ToString()), item.Content);
-    //    }
-
-    //    //Debug.WriteLine($"Set Content to: {item.Tag}");
-    //    //Debug.WriteLine($"NavView: {nvSample.SelectedItem}");
-    //}
-
     private AppWindow m_AppWindow;
 
     public Shell()
@@ -57,11 +24,36 @@ public sealed partial class Shell : Window
         this.InitializeComponent();
 
         Title = "Komorenga";
-        ExtendsContentIntoTitleBar = true;
-        SetTitleBar(AppTitleBar);
+
+        // Check to see if customization is supported.
+        // The method returns true on Windows 10 since Windows App SDK 1.2, and on all versions of
+        // Windows App SDK on Windows 11.
+        m_AppWindow = GetAppWindowForCurrentWindow();
+        if (AppWindowTitleBar.IsCustomizationSupported())
+        {
+            var titleBar = m_AppWindow.TitleBar;
+            // Hide default title bar.
+            titleBar.ExtendsContentIntoTitleBar = true;
+            
+            titleBar.ButtonBackgroundColor = new Color() { R = 31, G = 31, B = 31 };
+            //titleBar.ButtonHoverBackgroundColor = new Color() { R = 77, G = 77, B = 77 };
+        }
+        else
+        {
+            // In the case that title bar customization is not supported, hide the custom title bar
+            // element.
+            AppTitleBar.Visibility = Visibility.Collapsed;
+        }
 
         NavigationViewControl.SelectedItem = NavigationViewControl.MenuItems.OfType<NavigationViewItem>().First();
         ContentFrame.Navigate(typeof(Views.HomePage), null, new Microsoft.UI.Xaml.Media.Animation.EntranceNavigationTransitionInfo());
+    }
+
+    private AppWindow GetAppWindowForCurrentWindow()
+    {
+        IntPtr hWnd = WindowNative.GetWindowHandle(this);
+        WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+        return AppWindow.GetFromWindowId(wndId);
     }
 
     private void NavigationViewControl_DisplayModeChanged(NavigationView sender, NavigationViewDisplayModeChangedEventArgs args)
