a9b5ebac14066a3ff9c812b9e13065d4d5971a18
pphatt <vutienphat0809@gmail.com>
2023-08-19 19:35:47 +0700

    feat: add chapter count

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -109,4 +109,22 @@ namespace Komorenga.Utils
             throw new NotImplementedException();
         }
     }
+
+    class ParserChapterCount : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is int chapters)
+            {
+                return $"{chapters} Chapters";
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
